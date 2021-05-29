2b66cfc1012432ade1a3b2bb32368ed4c8064fc9
PlaustralCL <plaustralcl@gmail.com>
2021-05-29 18:28:17 -0400

    stock picker - work for provided test case

--- stock_picker.rb
+++ stock_picker.rb
@@ -2,17 +2,23 @@ stock_prices = [17, 3, 6, 9, 15, 8, 6, 1, 10]
 
 def stock_picker(stock_prices)
   profit = 0
+  buy_day = nil
+  sell_day = nil
   stock_prices.each_index do |index_i|
     stock_prices.each_index do |index_j|
-      if stock_prices[index_j] - stock_prices[index_i] > profit
+      if (stock_prices[index_j] - stock_prices[index_i] > profit) && (index_j > index_i)
         profit = stock_prices[index_j] - stock_prices[index_i]
+        sell_day = index_j
+        buy_day = index_i
       end
     end
     
   end
-  profit
+  stock_trade_info = {profit: profit, buy_day: buy_day, sell_day: sell_day}
 end
 
 puts "Stock prices: #{stock_prices}"
-p stock_picker(stock_prices)
-p stock_prices.sort
+stock_trade_info =  stock_picker(stock_prices)
+puts "Profit: #{stock_trade_info[:profit]}"
+puts "Buy day: #{stock_trade_info[:buy_day]}"
+puts "Sell day: #{stock_trade_info[:sell_day]}"

219c8693fb080f058e663c2dd61ef24bc195974e
PlaustralCL <plaustralcl@gmail.com>
2021-05-29 20:15:08 -0400

    stock picker - use hash instead of variables

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,19 +1,18 @@
 stock_prices = [17, 3, 6, 9, 15, 8, 6, 1, 10]
 
 def stock_picker(stock_prices)
-  profit = 0
-  buy_day = nil
-  sell_day = nil
+  # The initial value of -99 for profit is just a place holder
+  stock_trade_info = {profit: -99, buy_day: nil, sell_day: nil}
   stock_prices.each_index do |index_buy|
     stock_prices.each_index do |index_sell|
-      if (stock_prices[index_sell] - stock_prices[index_buy] > profit) && (index_sell > index_buy)
-        profit = stock_prices[index_sell] - stock_prices[index_buy]
-        sell_day = index_sell
-        buy_day = index_buy
+      if (stock_prices[index_sell] - stock_prices[index_buy] > stock_trade_info[:profit]) && (index_sell > index_buy)
+        stock_trade_info[:profit] = stock_prices[index_sell] - stock_prices[index_buy]
+        stock_trade_info[:sell_day] = index_sell
+        stock_trade_info[:buy_day] = index_buy
       end
     end
   end
-  stock_trade_info = {profit: profit, buy_day: buy_day, sell_day: sell_day}
+  stock_trade_info
 end
 
 puts "Stock prices: #{stock_prices}"

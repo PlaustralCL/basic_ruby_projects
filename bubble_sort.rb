d5292a7828d5eb26dd725cf0b91504604de51894
PlaustralCL <plaustralcl@gmail.com>
2021-05-30 11:06:19 -0400

    bublle sort - add comments

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,20 @@
 test_array = [4, 3, 78, 2, 0, 2]
 
 def bubble_sort(arr)
+  # variable to track if sorting is complete. Prevents needlessly looping through
+  # the array.
   still_sorting = false
+  # Number of times to go through the entire array
   for k in 1..(arr.length - 1)
+    # Loop through each element in the array. Starts at element 1 to prevent
+    # going off the end of the array. Compares i to i - 1. Since the largest
+    # unsorted value ends up at the proper positon on the right after each 
+    # complete pass through the array, the length of the loop can be shortened
+    # by one each time it is used.
     for i in 1..(arr.length - k)
       if arr[i] < arr[i - 1]
+        # temp values are used to prevent overwriting the values before they
+        # are swapped.
         large_temp = arr[i - 1]
         small_temp = arr[i]
         arr[i - 1] = small_temp
@@ -23,9 +33,6 @@ def bubble_sort(arr)
   # Return value
   arr
 end
-
-
-
 
 puts "Original Array: #{test_array}"
 puts "Sorted Array #{bubble_sort(test_array)}"

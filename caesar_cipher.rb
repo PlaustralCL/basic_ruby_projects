2ad01dc52c88c5a2767d93ffbcf8f22e91401585
PlaustralCL <plaustralcl@gmail.com>
2021-06-03 21:33:55 -0400

    caesar cipher - address rubocop issues

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,12 +1,12 @@
+# Shifts the string based on the key. The string is converted to an array and 
+# then each character is converted to the ascii number value. The key is 
+# then added (or subtracted) to the ascii number to give a new value. At the
+# end, the ascii numbers are converted back to letters. An array containing
+# shifted characters is returned.
+# @param str {String}
+# @param key {Integer}
+# @return {Array}
 def shift_string(str, key)
-  # Shifts the string based on the key. The string is converted to an array and 
-  # then each character is converted to the ascii number value. The key is 
-  # then added (or subtracted) to the ascii number to give a new value. At the
-  # end, the ascii numbers are converted back to letters. An array containing
-  # shifted characters is returned.
-  # @param str {String}
-  # @param key {Integer}
-  # @return {Array}
   a_ascii = 97 # ascii/ unicode value for lowercase a
   z_ascii = 122 # ascii/ unicode value for lowercase z
   alphabet_length = 26
@@ -41,13 +41,12 @@ def shift_string(str, key)
   shifted_array.map {|num| num.chr}
 end
 
-
+# Compares the case of the characters in the orignal user_string to
+# characters in the shifted_array and changes the shifted_array as needed
+# to match.
+# @param user_string [String] the original string from the user
+# @param shifted_array [Array] Array containing the characters after shifting
 def validate_case(user_string, shifted_array)
-  # Compares the case of the characters in the orignal user_string to
-  # characters in the shifted_array and changes the shifted_array as needed
-  # to match
-  # @param user_string [String] the original string from the user
-  # @param shifted_array [Array] Array containing the characters after shifting
   shifted_array.each_with_index do |chr, index|
     if user_string[index] == user_string[index].upcase
       chr.upcase!
@@ -55,40 +54,36 @@ def validate_case(user_string, shifted_array)
   end
 end
 
-
 def get_input
-  puts "This program will encrypt a string using a Caesar Cipher and the numeric key you provide."
-  print "Enter a string: "
+  puts 'This program will encrypt a string using a Caesar Cipher and the numeric key you provide.'
+  print 'Enter a string: '
   user_string = gets.chomp
 
-  print "Enter a number: "
+  print 'Enter a number: '
   user_key = gets.chomp.to_i
 
   input_array = [user_string, user_key]
 end
 
+# Normalizes the key to a number between 1 and 26
+# @param key [Integer] the key value
+# @return [Integer] value between 1 and 26 or -1 and -26
 def normalize_key(key)
-  # Normalizes the key to a number between 1 and 26
-  # @param key [Integer] the key value
-  # @return [Integer] value between 1 and 26 or -1 and -26
   positive_key = key.abs # the % function doesn't work well with negative numbers
-  if positive_key > 26
-    positive_key = positive_key % 26 
-  end
+  positive_key = positive_key % 26 if positive_key > 26
 
-  if key < 0
-    key = 0 - positive_key
+  if key.negative?
+    0 - positive_key
   else
-    key = positive_key
+    positive_key
   end
 end
 
-
 # Main code
-  input_array = get_input()
-  user_string = input_array[0]
-  user_key = input_array[1]
-  user_key = normalize_key(user_key)
-  encrpyted_array = shift_string(user_string, user_key)
-  encrpyted_array = validate_case(user_string, encrpyted_array)
-  puts "The encrypted string: #{encrpyted_array.join("")}"
+input_array = get_input()
+user_string = input_array[0]
+user_key = input_array[1]
+user_key = normalize_key(user_key)
+encrpyted_array = shift_string(user_string, user_key)
+encrpyted_array = validate_case(user_string, encrpyted_array)
+puts "The encrypted string: #{encrpyted_array.join("")}"

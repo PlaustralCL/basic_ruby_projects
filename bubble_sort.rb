d3eda1c4f4f46ca8c583936178d91d6d4b0039eb
PlaustralCL <plaustralcl@gmail.com>
2021-06-04 20:43:22 -0400

    bubble sort - address rubocop editorial comments

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 test_array = [4, 3, 78, 2, 0, 2]
 
 def bubble_sort(arr)
@@ -8,7 +10,7 @@ def bubble_sort(arr)
   for k in 1..(arr.length - 1)
     # Loop through each element in the array. Starts at element 1 to prevent
     # going off the end of the array. Compares i to i - 1. Since the largest
-    # unsorted value ends up at the proper positon on the right after each 
+    # unsorted value ends up at the proper positon on the right after each
     # complete pass through the array, the length of the loop can be shortened
     # by one each time it is used.
     for i in 1..(arr.length - k)
@@ -36,9 +38,9 @@ end
 
 puts "Original Array: #{test_array}"
 puts "Sorted Array #{bubble_sort(test_array)}"
-puts ""
-puts "Original Array: [9, 8, 7, 6, 5, 4]"
+puts ''
+puts 'Original Array: [9, 8, 7, 6, 5, 4]'
 puts "Sorted Array #{bubble_sort([9, 8, 7, 6, 5, 4])}"
-puts ""
-puts "Original Array: [1, 2, 3, 4, 5]"
+puts ''
+puts 'Original Array: [1, 2, 3, 4, 5]'
 puts "Sorted Array #{bubble_sort([1, 2, 3, 4, 5])}"

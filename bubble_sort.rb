62ab475013e3d1ef5e00b250eb04b0ee0af403a9
PlaustralCL <plaustralcl@gmail.com>
2021-06-04 21:13:50 -0400

    bubble - resolve more rubocop comments

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,35 +2,24 @@
 
 test_array = [4, 3, 78, 2, 0, 2]
 
-def bubble_sort(arr)
-  # variable to track if sorting is complete. Prevents needlessly looping through
-  # the array.
-  still_sorting = false
+# Loop through each element in the array. Starts at element 1 to prevent
+# going off the end of the array. Compares i to i - 1. Since the largest
+# unsorted value ends up at the proper positon on the right after each
+# complete pass through the array, the length of the loop can be shortened
+# by one each time it is used.
+def bubble_sort(arr, sorted: true)
   # Number of times to go through the entire array
   for k in 1..(arr.length - 1)
-    # Loop through each element in the array. Starts at element 1 to prevent
-    # going off the end of the array. Compares i to i - 1. Since the largest
-    # unsorted value ends up at the proper positon on the right after each
-    # complete pass through the array, the length of the loop can be shortened
-    # by one each time it is used.
+
     for i in 1..(arr.length - k)
       if arr[i] < arr[i - 1]
-        # temp values are used to prevent overwriting the values before they
-        # are swapped.
-        large_temp = arr[i - 1]
-        small_temp = arr[i]
-        arr[i - 1] = small_temp
-        arr[i] = large_temp
-        still_sorting = true
+        arr[i], arr[i - 1] = arr[i - 1], arr[i]
+        sorted = false
       end
     end
 
-    if still_sorting == false
-      break
-    else
-      k += 1
-      still_sorting = false
-    end
+    break if sorted
+
   end
   # Return value
   arr

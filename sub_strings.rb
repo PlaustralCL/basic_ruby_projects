4bbc444d262116a899cf50ebcb932b0dfa710953
PlaustralCL <plaustralcl@gmail.com>
2021-06-04 20:31:15 -0400

    sub strings - address rubocop

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,30 +1,24 @@
+# frozen_string_literal: true
+
 # Insight in reducing an array to a hash, particullary the need to explicity
 # return the accumlator hash came from the following sites:
 # https://bytesize.cultivatehq.com/ruby/2019/01/03/ruby-reduce-to-object-gotcha.html
 # https://technology.customink.com/blog/2014/10/14/better-hash-injection-using-each-with-object/
 
 def substrings(str, dictionary)
-  array_from_str = str.split(" ")
-  dictionary.reduce({}) do |memo, dict_word|
+  array_from_str = str.split(' ')
+  dictionary.each_with_object({}) do |dict_word, memo|
     array_from_str.each do |str_word|
       if str_word.downcase.include?(dict_word)
-        memo.has_key?(dict_word) ? memo[dict_word] += 1 : memo[dict_word] = 1
-        # if memo.has_key?(dict_word)
-        #   memo[dict_word] += 1
-        # else
-        #   memo[dict_word] = 1
-        # end
+        memo.key?(dict_word) ? memo[dict_word] += 1 : memo[dict_word] = 1
       end
     end
-    # memo must be returned to maintain the hash
-    memo
   end
 end
 
-
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-puts "below"
-puts substrings("below", dictionary)
-puts ""
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+puts 'below'
+puts substrings('below', dictionary)
+puts ''
 puts "Howdy partner, sit down! How's it going?"
 puts substrings("Howdy partner, sit down! How's it going?", dictionary)
